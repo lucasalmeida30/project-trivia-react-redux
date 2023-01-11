a4b56cafc4716b0dc0da57957136dff5b9c1533f
rosamenezes <g.rosamenezes1@gmail.com>
2023-01-11 16:25:21 -0300

    REQ1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validButton());
+  };
+
+  validButton = () => {
+    const minNumber = 0;
+    const regexEmail = /\S+@\S+\.\S+/;
+    const { name, email } = this.state;
+    if (regexEmail.test(email) && name.length > minNumber) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    }
+  };
+
+  render() {
+    const { name, email, isButtonDisabled } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          name="name"
+          data-testid="input-player-name"
+          onChange={ this.handleChange }
+          value={ name }
+          placeholder="Nome"
+        />
+        <input
+          type="email"
+          name="email"
+          data-testid="input-gravatar-email"
+          onChange={ this.handleChange }
+          value={ email }
+          placeholder="E-mail"
+        />
+        <button
+          type="button"
+          disabled={ isButtonDisabled }
+          data-testid="btn-play"
+        >
+          Play
+        </button>
+
+      </div>
+    );
+  }
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+// import { combineReducers } from 'redux';
+// import wallet from './wallet';
+// import user from './user';''
+
+// const rootReducer = combineReducers({
+//   user,
+//   wallet,
+// });
+
+// export default rootReducer;
