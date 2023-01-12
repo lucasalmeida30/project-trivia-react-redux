f03f40365a05b7f76c73f88ae54be5a492da401e
lucasalmeida30 <lucas_almeida12@icloud.com>
2023-01-12 12:32:12 -0300

    adiciona requisito 5

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Game extends Component {
   render() {
     return (
-      <div>Game Trivia</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
