eea7954c95954829bab1d42fd55f116e1d8939f1
lucasalmeida30 <lucas_almeida12@icloud.com>
2023-01-14 13:07:51 -0300

    adiciona requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div data-testid="feedback-text">
-        FEEDBACK
+        <Header />
       </div>
     );
   }
